fad576dc627fb5b3645404d6a7cbc7ac31744ecf
Kelly Griffin <griffin.k.r.1988@gmail.com>
2026-08-15 04:35:40 -0400

    Add mahogany seed and pod items

--- Treet/src/main/java/ca/goldengriffingaming/treet/GggTreet.java
+++ Treet/src/main/java/ca/goldengriffingaming/treet/GggTreet.java
@@ -2,7 +2,17 @@ package ca.goldengriffingaming.treet;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.core.registries.Registries;
+
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +25,15 @@ public class GggTreet implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private static final ResourceKey<LootTable> MAHOGANY_LEAVES_LOOT_TABLE =
+        ResourceKey.create(
+                Registries.LOOT_TABLE,
+                Identifier.fromNamespaceAndPath(
+                        "biomesoplenty",
+                        "blocks/mahogany_leaves"
+                )
+        );
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -23,7 +42,25 @@ public class GggTreet implements ModInitializer {
 
 	ModItems.initialize();
 
-		LOGGER.info("Hello Fabric world!");
+	LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+    if (MAHOGANY_LEAVES_LOOT_TABLE.equals(key)) {
+
+        LootPool.Builder seedPool = LootPool.lootPool()
+                .setRolls(ConstantValue.exactly(1.0F))
+                .when(LootItemRandomChanceCondition.randomChance(0.05F))
+                .add(LootItem.lootTableItem(ModItems.MAHOGANY_SEEDS));
+
+        LootPool.Builder podPool = LootPool.lootPool()
+                .setRolls(ConstantValue.exactly(1.0F))
+                .when(LootItemRandomChanceCondition.randomChance(0.02F))
+                .add(LootItem.lootTableItem(ModItems.MAHOGANY_SEED_POD));
+
+        tableBuilder.withPool(seedPool);
+        tableBuilder.withPool(podPool);
+    }
+});
+
+		LOGGER.info("GGG-Treet is a work in progress.");
 	}
 
 	public static Identifier id(String path) {
