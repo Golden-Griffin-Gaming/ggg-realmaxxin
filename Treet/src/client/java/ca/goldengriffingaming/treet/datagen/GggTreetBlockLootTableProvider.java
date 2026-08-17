f148839c0e483ac68dd5676579b70cd0f56c0193
Kelly Griffin <griffin.k.r.1988@gmail.com>
2026-08-16 20:36:42 -0400

    Add Cypress building blocks and datagen loot tables

---
+++ Treet/src/client/java/ca/goldengriffingaming/treet/datagen/GggTreetBlockLootTableProvider.java
@@ -0,0 +1,41 @@
+package ca.goldengriffingaming.treet.datagen;
+
+import ca.goldengriffingaming.treet.ModBlocks;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootSubProvider;
+
+import net.minecraft.core.HolderLookup;
+
+import java.util.concurrent.CompletableFuture;
+
+public class GggTreetBlockLootTableProvider extends FabricBlockLootSubProvider {
+
+    protected GggTreetBlockLootTableProvider(
+            FabricPackOutput dataOutput,
+            CompletableFuture<HolderLookup.Provider> registryLookup
+    ) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+
+        dropSelf(ModBlocks.CYPRESS_LOG);
+        dropSelf(ModBlocks.STRIPPED_CYPRESS_LOG);
+        dropSelf(ModBlocks.CYPRESS_WOOD);
+        dropSelf(ModBlocks.STRIPPED_CYPRESS_WOOD);
+        dropSelf(ModBlocks.CYPRESS_PLANKS);
+
+        add(
+                ModBlocks.CYPRESS_SLAB,
+                createSlabItemTable(ModBlocks.CYPRESS_SLAB)
+        );
+
+        dropSelf(ModBlocks.CYPRESS_STAIRS);
+        dropSelf(ModBlocks.CYPRESS_FENCE);
+        dropSelf(ModBlocks.CYPRESS_FENCE_GATE);
+        dropSelf(ModBlocks.CYPRESS_BUTTON);
+        dropSelf(ModBlocks.CYPRESS_PRESSURE_PLATE);
+    }
+}
--- Treet/src/main/java/ca/goldengriffingaming/treet/ModBlocks.java
+++ Treet/src/main/java/ca/goldengriffingaming/treet/ModBlocks.java
@@ -14,6 +14,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.block.PressurePlateBlock;
 
 import java.util.function.Function;
 
@@ -50,6 +58,43 @@ public class ModBlocks {
         "cypress_planks",
         Block::new,
         BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS),
+        true
+    );
+
+    public static final Block CYPRESS_SLAB = register(
+        "cypress_slab",
+        SlabBlock::new,
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SLAB),
+        true
+    );
+    public static final Block CYPRESS_STAIRS = register(
+        "cypress_stairs",
+        properties -> new StairBlock(CYPRESS_PLANKS.defaultBlockState(), properties),
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_STAIRS),
+        true
+    );
+    public static final Block CYPRESS_FENCE = register(
+        "cypress_fence",
+        FenceBlock::new,
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_FENCE),
+        true
+    );
+    public static final Block CYPRESS_FENCE_GATE = register(
+        "cypress_fence_gate",
+        properties -> new FenceGateBlock(WoodType.OAK, properties),
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_FENCE_GATE),
+        true
+    );
+    public static final Block CYPRESS_BUTTON = register(
+        "cypress_button",
+        properties -> new ButtonBlock(BlockSetType.OAK, 30, properties),
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_BUTTON),
+        true
+    );
+    public static final Block CYPRESS_PRESSURE_PLATE = register(
+        "cypress_pressure_plate",
+        properties -> new PressurePlateBlock(BlockSetType.OAK, properties),
+        BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PRESSURE_PLATE),
         true
     );
 
@@ -115,6 +160,12 @@ public class ModBlocks {
                         entries.accept(CYPRESS_WOOD.asItem());
                         entries.accept(STRIPPED_CYPRESS_WOOD.asItem());
                         entries.accept(CYPRESS_PLANKS.asItem());
+                        entries.accept(CYPRESS_SLAB.asItem());
+                        entries.accept(CYPRESS_STAIRS.asItem());
+                        entries.accept(CYPRESS_FENCE.asItem());
+                        entries.accept(CYPRESS_FENCE_GATE.asItem());
+                        entries.accept(CYPRESS_BUTTON.asItem());
+                        entries.accept(CYPRESS_PRESSURE_PLATE.asItem());
                      });
     }
 }
